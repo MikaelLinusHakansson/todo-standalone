bc8febe95dfc511ddaa98983d22b18fcd93b9fae
linushakansson <linus.hakansson@skolon.com>
2023-11-24 15:00:35 +0100

    added bootstrap to the application

--- todo-app-standalone/src/main.js
+++ todo-app-standalone/src/main.js
@@ -4,6 +4,9 @@ import { createPinia } from 'pinia'
 import i18n from './configurations/i18n.js'
 import App from './App.vue'
 
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 const pinia = createPinia();
 const app = createApp(App);
 
