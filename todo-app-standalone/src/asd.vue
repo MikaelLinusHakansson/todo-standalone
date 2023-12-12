cabc0c9c33b897c8690526256b45dbe21c760b7a
linushakansson <linus.hakansson@skolon.com>
2023-12-12 16:40:47 +0100

    changed name

--- todo-app-standalone/src/asd.vue
+++ todo-app-standalone/src/asd.vue
@@ -1,14 +1,19 @@
 <template>
-
+    <NotApp></NotApp>
 </template>
 
 <script>
 // TODO Importera App.vue här
+import NotApp from "@/components/NotApp.vue" 
     export default {
+        components: {
+            NotApp,
+        },
+        
         data() {
             return {
 
             }
-        }
+        },
     }
 </script>
