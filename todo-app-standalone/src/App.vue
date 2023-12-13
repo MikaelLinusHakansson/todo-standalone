21f39627ec6cb2e0f903c3d5a84c3224b6c3e8fc
linushakansson <linus.hakansson@skolon.com>
2023-12-13 08:45:32 +0100

    created TodoPage.vue and moved App.vue content. App.vue is now empty

--- todo-app-standalone/src/App.vue
+++ todo-app-standalone/src/App.vue
@@ -1,94 +1,17 @@
 <template>
-  <div class="container mt-4">
-    <task-title-header />
-
-    <task-change-language />
-
-    <task-form />
-
-    <task-controls 
-      :toggle-all="toggleAll"
-      :toggle-completed="toggleCompleted"
-      :toggle-data-table="toggleDataTable">
-    </task-controls>
-
-    <div 
-      class="mb-3" 
-      :hidden="visableAllTasks">
-        <task-all-tasks-list
-          class="list-group d-flex" 
-          :isVisable="isVisable">
-        </task-all-tasks-list>
-    </div>
-
-    <task-data-table :hidden="visableDataTable" />
-
-    <div 
-      :hidden="this.visableCompleted">
-        <task-completed-list />
-    </div>
+  <div>
+    <todo-page />
   </div>
 </template>
 
 <script>
-// TODO 1. Skapa ny komponent som blir blir nya App.vue, med andra ord byt namn på denna App.vue till ett annat passande namn. Skapa en ny komponent som kommer att importera denna komponenten och visa den i sin template.
-// TODO 2. I storen se till att byta ut getData() anropen som görs överallt till att lokalt manipulera listan med tasks för att undvika onödiga API anrop. 
-import TaskChangeLanguage from "@/components/TaskChangeLanguage.vue";
-import TaskTitleHeader from "./components/TaskTitleHeader.vue";
-import TaskForm from "@/components/taskForm.vue";
-import TaskControls from "@/components/TaskControls.vue";
-import TaskCompletedList from "@/components/TaskCompletedList.vue";
-import TaskAllTasksList from "./components/TaskAllTasksList.vue";
-import TaskDataTable from "@/components/TaskDataTable.vue";
+import TodoPage from "./components/TodoPage.vue";
 
 export default {
   components: {
-    TaskTitleHeader,
-    TaskChangeLanguage,
-    TaskForm,
-    TaskControls,
-    TaskAllTasksList,
-    TaskDataTable,
-    TaskCompletedList,
+    TodoPage
 },
 
-  data() {
-    return {
-      isVisable: true,
-      visableCompleted: true,
-      visableAllTasks: false,
-      visableDataTable: true,
-    }
-  },
-
-  provide() {
-    return {
-      i18n: this.$i18n
-    }
-  },
-
-  computed: {},
-
-  methods: {
-    toggleCompleted() {
-      this.visableCompleted = !this.visableCompleted
-    },
-
-    toggleAll() {
-      this.visableAllTasks = !this.visableAllTasks
-    },
-
-    toggleDataTable() {
-      this.visableDataTable = !this.visableDataTable
-    },
-    
-    ToggleEdit() {
-        this.isVisable = !this.isVisable
-    },
-
-    changeTheLanguage(locale) {
-      this.$i18n.locale = locale
-    },
-  }
+  data() {return {}},
 }
 </script>
---
+++ todo-app-standalone/src/components/TodoPage.vue
@@ -0,0 +1,90 @@
+<template>
+    <div class="container mt-4">
+    <task-title-header />
+
+    <task-change-language />
+
+    <task-form />
+
+    <task-controls 
+      :toggle-all="toggleAll"
+      :toggle-completed="toggleCompleted"
+      :toggle-data-table="toggleDataTable">
+    </task-controls>
+
+    <div 
+      class="mb-3" 
+      :hidden="visableAllTasks">
+        <task-all-tasks-list
+          class="list-group d-flex" 
+          :isVisable="isVisable">
+        </task-all-tasks-list>
+    </div>
+
+    <task-data-table :hidden="visableDataTable" />
+
+    <div 
+      :hidden="this.visableCompleted">
+        <task-completed-list />
+    </div>
+  </div>
+</template>
+
+<script>
+import TaskChangeLanguage from "@/components/TaskChangeLanguage.vue";
+import TaskTitleHeader from "@/components/TaskTitleHeader.vue";
+import TaskForm from "@/components/taskForm.vue";
+import TaskControls from "@/components/TaskControls.vue";
+import TaskCompletedList from "@/components/TaskCompletedList.vue";
+import TaskAllTasksList from "@/components/TaskAllTasksList.vue";
+import TaskDataTable from "@/components/TaskDataTable.vue";
+
+export default {
+    components: {
+        TaskTitleHeader,
+        TaskChangeLanguage,
+        TaskForm,
+        TaskControls,
+        TaskAllTasksList,
+        TaskDataTable,
+        TaskCompletedList,
+    },
+        
+    data() {
+        return {
+            isVisable: true,
+            visableCompleted: true,
+            visableAllTasks: false,
+            visableDataTable: true,
+        }
+    },
+
+    provide() {
+        return {
+        i18n: this.$i18n
+        }
+    },
+
+    methods: {
+        toggleCompleted() {
+            this.visableCompleted = !this.visableCompleted
+    },
+
+        toggleAll() {
+            this.visableAllTasks = !this.visableAllTasks
+        },
+
+        toggleDataTable() {
+        this.visableDataTable = !this.visableDataTable
+        },
+    
+        ToggleEdit() {
+            this.isVisable = !this.isVisable
+        },
+
+        changeTheLanguage(locale) {
+        this.$i18n.locale = locale
+        },
+    }
+}
+</script>
