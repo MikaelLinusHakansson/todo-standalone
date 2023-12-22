d026c8769040404ced69f37535645ec552592ef5
linush <linsahakansson@gmail.com>
2023-12-22 15:01:47 +0100

    adding store to the user

--- todo-app-standalone/src/stores/userStore.js
+++ todo-app-standalone/src/stores/userStore.js
@@ -1,1 +1,25 @@
 import { defineStore } from "pinia";
+
+export const userStore = defineStore("userStore", {
+    state: () => {
+        return {
+            accessToken: "",
+            user: null
+        }
+    },
+
+    actions: {
+        setBearerToken(BearerToken) {
+            this.accessToken = BearerToken;
+        },
+
+        setUser(user) {
+            this.user = user;
+        },
+
+        logout() {
+            this.accessToken = ''
+            this.user = null
+        }
+    }
+});
