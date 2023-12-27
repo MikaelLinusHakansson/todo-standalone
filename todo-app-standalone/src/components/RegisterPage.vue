23037f5012e05fb6ea7dfd45dd7337c7d0301a98
linush <linus_hakansson@outlook.com>
2023-12-27 12:00:48 +0100

    added another dive omega lul

--- todo-app-standalone/src/components/RegisterPage.vue
+++ todo-app-standalone/src/components/RegisterPage.vue
@@ -9,15 +9,14 @@
                             <form>
                                 <div>
                                    <div class="mb-3">
-                                    <label for="username" id="username" class="form-label">Username</label>
-                                    <InputText v-model="username" id="username" class="form-control">username</InputText>
-                                </div>
-                                 <div class="mb-3">
+                                        <label for="username" id="username" class="form-label">Username</label>
+                                        <InputText v-model="username" id="username" class="form-control">username</InputText>
+                                    </div>
+                                    <div class="mb-3">
                                 <!-- <label for="password" id="password" class="form-label">Password</label> -->
-                                    <Password v-model="password" toggleMask promptLabel="Enter your password" weakLabel="Very Weak" mediumLabel="Medium" strongLabel="Strong">password</Password>
-                                </div> 
+                                        <Password v-model="password" toggleMask promptLabel="Enter your password" weakLabel="Very Weak" mediumLabel="Medium" strongLabel="Strong">password</Password>
+                                    </div> 
                                 </div>
-                               
                                 <Button @click="registerUser" class="btn btn-primary w-100">Submit</Button> 
                             </form>
                         </template>
