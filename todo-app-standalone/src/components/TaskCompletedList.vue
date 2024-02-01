855a899e2ed186dbc0cf020a712e0e0edbcad7e0
linush <linus_hakansson@outlook.com>
2024-02-01 15:24:45 +0100

    fixed completed task to look the same as everythign else

--- todo-app-standalone/src/components/TaskCompletedList.vue
+++ todo-app-standalone/src/components/TaskCompletedList.vue
@@ -1,16 +1,16 @@
 <template>
     <table>
         <div>
-            <span class="text-primary fw-bold">
+            <span class="completed-tasks-header">
                     {{ $t('done') }}:
             </span>
 
             <ul
-                class="list-group">
+                class="task-list">
                 <li
                     v-for="(completedTask) in this.completedTasksGetters" 
                     :key="completedTask.id" 
-                    class="list-group-item">
+                    class="list-item task-completed">
                         {{ completedTask.name }}
                         {{ completedTask.date }}
                 </li>
@@ -34,3 +34,31 @@ export default {
     },
 }
 </script>
+
+<style scoped>
+
+.completed-tasks-header {
+    color: #0DC0F0;
+    font-weight: bold;
+}
+.task-list {
+    list-style-type: none;
+    padding: 0;
+}
+
+.list-item {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0.5rem;
+    background-color: #f8f9fa;
+    border-bottom: 1px solid #e7e7e7;
+    border-radius: 8px;
+    margin-bottom: 5px;
+
+}
+
+.task-name, .task-date {
+    padding: 0.5rem;
+}   
+</style>
