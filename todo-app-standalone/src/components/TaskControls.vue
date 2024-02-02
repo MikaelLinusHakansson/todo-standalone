de85f58bf6ccf54ca711f4d6c4d5a5c49a9a00bf
linush <linus_hakansson@outlook.com>
2024-02-02 09:37:02 +0100

    replaced the show all switch button

--- todo-app-standalone/src/components/TaskControls.vue
+++ todo-app-standalone/src/components/TaskControls.vue
@@ -3,21 +3,12 @@
         <div class="mb-2">
             <div class="d-flex flex-column">
                 <div class="d-flex">
-                    <label
-                        class="mr-2"
-                        for="checkedAll">
-                            {{ $t('showAll') }}
-                    </label>
 
-                    <InputSwitch
-                        class="m-1"
-                        input-id="checkedAll"
-                        v-model="checkedToggleAll" 
-                        @click="toggleAll()" >
-                            <span 
-                                id="checkedAll">
-                            </span>
-                    </InputSwitch>
+                    <label class="switch">
+                        <input type="checkbox" v-model="checkedToggleAll" @change="toggleAll()" />
+                        <span class="slider round" />
+                    </label>
+                        {{ $t('showAll') }}
                 </div>
 
                 <div class="d-flex mt-1">
@@ -27,7 +18,7 @@
                             {{ $t('showDone') }}
                     </label>
 
-                    <InputSwitch 
+                    <InputSwitch
                         class="m-1"
                         input-id="checkedCompleted" 
                         v-model="checkedCompleted" 
@@ -106,3 +97,68 @@ export default {
     }
 }
 </script>
+
+<style scoped>
+
+.switch {
+    position: relative;
+    display: inline-block;
+    width: 40px;
+    height: 20px;
+    margin: 1rem;
+
+}
+.switch input {
+    opacity: 0;
+    width: 0;
+    height: 0;
+}
+
+.slider {
+    position: absolute;
+    cursor: pointer;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: #ccc;
+    -webkit-transition: .4s;
+    transition: .4s;
+    border-radius: 20px;
+}
+
+.slider:before {
+    position:absolute;
+    content: "";
+    height: 16px;
+    width: 16px;
+    left: 2px;
+    bottom: 2px;
+    background-color: white;
+    -webkit-transition: .4s;
+    transition: .4s;
+    border-radius: 50%;
+}
+
+input:checked + .slider {
+    background-color: #0DC0F0;
+}
+
+input:focus + .slider {
+    box-shadow: 0 0 1px #2196F3;
+}
+
+input:checked + .slider:before {
+    -webkit-transform: translateX(26px);
+    -ms-transform: translateX(26px);
+    transform: translateX(26px);
+}
+
+.slider.round {
+    border-radius: 34px;
+}
+
+.slider.round:before {
+    border-radius: 50%;
+}
+</style>
