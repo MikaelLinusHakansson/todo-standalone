ea0533d7d1e5f886e0ea14ee3ebcb0cc689ce078
linush <linus_hakansson@outlook.com>
2024-04-24 14:22:29 +0200

    OCD stuff

--- todo-app-standalone/src/api/services/apiService.js
+++ todo-app-standalone/src/api/services/apiService.js
@@ -1,44 +1,44 @@
 export default class ApiService {
-    constructor(baseURL) {
-        this.baseURL = baseURL
+  constructor(baseURL) {
+    this.baseURL = baseURL
+  }
+
+  async makeRequest(method, endpoint, data = null, accessToken = null , returnFullResponse = null) {
+    const url = `${this.baseURL}${endpoint}`
+    const headers = {
+      'Content-Type': 'application/json',
     }
 
-    async makeRequest(method, endpoint, data = null, accessToken = null , returnFullResponse = null) {
-        const url = `${this.baseURL}${endpoint}`
-        const headers = {
-            'Content-Type': 'application/json',
-        }
+    if (accessToken) {
+      headers['Authorization'] = `Bearer ${accessToken}`
+    }
 
-        if (accessToken) {
-            headers['Authorization'] = `Bearer ${accessToken}`
-        }
+    const config = {
+      method: method,
+      headers: headers,
+    }
 
-        const config = {
-            method: method,
-            headers: headers,
-        }
-
-        if (data) {
-            config.body = JSON.stringify(data)
-        }
+    if (data) {
+      config.body = JSON.stringify(data)
+    }
         
-        try {
-                const response = await fetch(url, config)
+    try {
+      const response = await fetch(url, config)
             
-                if (!response.ok) {
-                    throw new Error (`HTTP error! Status: ${response.status}`)
-                }
-                
-                if (method === 'DELETE') {
-                    return null
-                }
-                
-                return returnFullResponse ? response : await response.json()
-            }
-            
-            catch (error) {
-                console.error(`Error making ${method} request to ${url}`, error.message)
-                throw error
-            }
+      if (!response.ok) {
+          throw new Error (`HTTP error! Status: ${response.status}`)
+      }
+
+      if (method === 'DELETE') {
+          return null
+      }
+
+      return returnFullResponse ? response : await response.json()
     }
+            
+    catch (error) {
+      console.error(`Error making ${method} request to ${url}`, error.message)
+      throw error
+    }
+  }
 }
